965ae00e676027fe29506c200e030a5cde243584
pradeep9955 <pradeeproy732@gmail.com>
2023-02-22 13:10:47 +0530

    create file/multiple files

--- data.js
+++
@@ -1,7 +0,0 @@
-const data=[
-    {name: 'Pradeep Kumar', email: 'pradeeproy732@gmail.com', contect:'88997621'},
-    {name: 'sam', email: 'sam732@gmail.com', contacat: '88976'},
-    {name: 'Peter', email: 'peter@gmail.com'}
-];
-
-module.exports=data;
--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const http = require('http');
-const data = require('./data');
+const fs = require('fs');
+const path = require('path');
 
-http.createServer((req, res)=>{
-    res.writeHead(200, {'content-type':'application\json'});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(4000);
+//path set-> files folder
+const dirPath = path.join(__dirname, 'files');
+
+//multiple file create in files folder
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(dirPath+`/hello${i}.txt`, "this is a simple file");
+// }
+
+//file reads
+fs.readdir(dirPath,(err, files)=>{
+    //files name in array formet
+    //console.log(files)
+
+    // files name-> indivisualy
+    files.forEach((item)=>{
+        console.log("file name is: ", item);
+    })
+})
+
+
+
+// console.log("hello");
