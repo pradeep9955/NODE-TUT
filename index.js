990d871479c9c69130e4bde072f606db141825ab
pradeep9955 <pradeeproy732@gmail.com>
2023-02-22 18:06:33 +0530

    asynchronous drowback handle

--- index.js
+++ index.js
@@ -8,11 +8,28 @@
 // console.log('complete exe...');
 
 //Drowback
+// a=10;
+// b=20;
+
+// setTimeout(()=>{
+//     b=30;
+// },2000);
+
+// console.log(a+b);
+
+//Handle DrowBack
 a=10;
 b=20;
 
-setTimeout(()=>{
-    b=30;
-},2000);
+let WatingData = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve(40)
+    },2000);
+})
 
-console.log(a+b);
+WatingData.then((data)=>{
+    b=data;
+    console.log(a+b);
+})
+
+
