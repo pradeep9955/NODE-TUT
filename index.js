dd72c44250e4054da5fa35ea4adb13001dbb7463
pradeep9955 <pradeeproy732@gmail.com>
2023-02-23 12:38:17 +0530

    create 404 page and remove extension from url

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const path = require('path');
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
+//app.use(express.static(publicPath));
+app.get('/home', (req, res)=>{
+    res.sendFile(`${publicPath}/home.html`);
+});
 
-app.use(express.static(publicPath));
+app.get('/about', (req,res)=>{
+    res.sendFile(`${publicPath}/about.html`);
+});
+
+app.get('*', (req,res)=>{
+    res.sendFile(`${publicPath}/404.html`);
+});
 
 app.listen(4000);
