56fc7712a8ee444cd3a78ac5de4d549683c2c91f
pradeep9955 <pradeeproy732@gmail.com>
2023-02-23 17:21:59 +0530

    application-level middlewere

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const publicPath = path.join(__dirname, 'public');
 
-//ejs template
-app.set('view engine', 'ejs');
-
-app.get('/home', (req, res)=>{
-    res.sendFile(`${publicPath}/home.html`);
-});
-
-app.get('/profile', (req, res)=>{
-    const user={
-        name: "pradeep",
-        email: 'pradeep@gmail.com',
-        city: "patna",
-        skills: ['php','js','c++', 'java', 'node']
+const reqFilter=(req, res, next)=>{
+    if(!req.query.age){
+        res.send("Please provide age");
     }
-    res.render('profile', {user});
+    else if(req.query.age<18){
+        res.send("You can't access this page");
+    }
+    else{
+        next();
+    }
+};
+
+app.use(reqFilter);
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to Home Page');
 });
 
-app.get('/login', (req,res)=>{
-    res.render('login');
-});
-
-app.get('/about', (req,res)=>{
-    res.sendFile(`${publicPath}/about.html`);
-});
-
-app.get('*', (req,res)=>{
-    res.sendFile(`${publicPath}/404.html`);
+app.get('/users', (req, res)=>{
+    res.send('Welcome to Users Page');
 });
 
 app.listen(4000);
