14a4d670b8c46ad3e37b1ca90c80741c82c79aa8
pradeep9955 <pradeeproy732@gmail.com>
2023-02-24 19:38:59 +0530

    connect mongoDB with nodejs

--- index.js
+++ index.js
@@ -1,96 +1,14 @@
-// const express = require('express');
-// const app = express();
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017';
+const database = 'e-com';
+const client = new MongoClient(url);
 
-// const reqFilter=(req, res, next)=>{
-//     if(!req.query.age){
-//         res.send("Please provide age");
-//     }
-//     else if(req.query.age<18){
-//         res.send("You can't access this page");
-//     }
-//     else{
-//         next();
-//     }
-// };
+async function getData(){
+    let result = await client.connect();
+    let db = result.db(database);
+    let collection = db.collection('products');
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
 
-
-
-// app.get('/', (req, res)=>{
-//     res.send('Welcome to Home Page');
-// });
-
-//Single Routed Middlewere
-// app.get('/users', reqFilter, (req, res)=>{
-//     res.send('Welcome to Users Page');
-// });
-
-// app.get('/about', (req, res)=>{
-//     res.send('Welcome to About Page');
-// });
-
-
-// app.listen(4000);
-
-
-//Multi Routed Middlewere
-// const express = require('express');
-// const app = express();
-
-// const reqFilter=(req, res, next)=>{
-//     if(!req.query.age){
-//         res.send("Please provide age");
-//     }
-//     else if(req.query.age<18){
-//         res.send("You can't access this page");
-//     }
-//     else{
-//         next();
-//     }
-// };
-
-
-
-// app.get('/', (req, res)=>{
-//     res.send('Welcome to Home Page');
-// });
-
-// app.get('/users', reqFilter, (req, res)=>{
-//     res.send('Welcome to Users Page');
-// });
-
-// app.get('/about',reqFilter, (req, res)=>{
-//     res.send('Welcome to About Page');
-// });
-
-
-// app.listen(4000);
-
-
-
-// Group Rout Middlewere
-const express = require('express');
-const reqFilter = require('./middlewere');
-const app = express();
-const route = express.Router();
-
-route.use(reqFilter);
-
-app.get('/', (req, res)=>{
-    res.send('Welcome to Home Page');
-});
-
-app.get('/users', (req, res)=>{
-    res.send('Welcome to Users Page');
-});
-
-route.get('/about', (req, res)=>{
-    res.send('Welcome to About Page');
-});
-
-route.get('/contact', (req, res)=>{
-    res.send('Welcome to Contact Page');
-});
-
-app.use('/',route);
-
-app.listen(4000);
+getData();
