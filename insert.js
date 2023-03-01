b4f787b7901abaa091fb141c61dfc2eb068dc984
pradeep9955 <pradeeproy732@gmail.com>
2023-03-01 13:16:11 +0530

    insert multiple data

---
+++ insert.js
@@ -0,0 +1,16 @@
+const dbConnect = require('./mongodb');
+
+const insert = async ()=>{
+    const db=await dbConnect();
+    const result = db.insertMany(
+        [
+            {name:'max 1',brand:'micromax',price:220,category:'mobile'},
+            {name:'max 2',brand:'micromax',price:320,category:'mobile'},
+            {name:'max 3',brand:'micromax',price:420,category:'mobile'}
+        ]
+        );
+        if((await result).acknowledged){
+            console.log("data inserted")
+        }
+}
+insert();
