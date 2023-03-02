16f442d4a95c424dbaf6f8af2f19e6f34e25986b
pradeep9955 <pradeeproy732@gmail.com>
2023-03-02 13:27:01 +0530

    update onr ar many recordes

--- mongodb.js
+++ mongodb.js
@@ -7,8 +7,6 @@ async function dbConnect(){
     let result = await client.connect();
     let db = result.db(database);
     return db.collection('products');
-    // let response = await collection.find({name: 'u10'}).toArray();
-    // console.log(response);
 }
 
 module.exports= dbConnect;
