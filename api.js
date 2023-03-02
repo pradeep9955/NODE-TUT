0a64db562649e517a08a93186f73cfe8d29840f6
pradeep9955 <pradeeproy732@gmail.com>
2023-03-02 19:35:46 +0530

    data send ans update through postman into nodejs and in  mongodb database

--- api.js
+++ api.js
@@ -4,17 +4,28 @@ const app = express();
 
 app.use(express.json());
 
+// GET
 app.get('/', async (req, resp)=>{
     let data = await dbConnect();
     data = await data.find().toArray();
     resp.send(data);
 });
 
+//POST
 app.post('/', async (req, resp)=>{
     let data = await dbConnect();
     let result = await data.insertOne(req.body);
     resp.send(result);
 });
 
+//PUT
+app.put('/', async (req, resp)=>{
+    let data = await dbConnect();
+    let result = data.updateOne(
+        {name:req.body.name},
+        {$set:req.body}
+        )
+    resp.send({result:"update"})
+});
 
 app.listen(5000);
